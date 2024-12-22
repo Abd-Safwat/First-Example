1780680bda5e999ef147356c93235ea555f233ff
‪Abdelrahman Safwat‬‏ <100530056+Abd-Safwat@users.noreply.github.com>
2024-12-22 18:12:04 +0200

    Add files via upload

---
+++ public/js/scrip.js
@@ -0,0 +1,9 @@
+function changeColor() { 
+    let pco01 = document.getElementById("ex01"); 
+    let pco02 = document.getElementById("ex02"); 
+    let pco03 = document.getElementById("ex03"); 
+    pco01.style.color = ' #D21312';
+    pco02.style.color = ' #ECDFCC';
+    pco03.style.color =' #070A52';
+}
+changeColor();
